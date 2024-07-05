2a27018fc7c61ca9a01e2b5d2d11452882b4d0a4
Marek Sagan <marek.sagan@protonmail.com>
2024-07-05 09:38:42 +0100

    Filesystem change notifier configuration added

---
+++ src/main/java/com/recruitment/temperatures/ServerStartupChangeDetector.java
@@ -0,0 +1,39 @@
+package com.recruitment.temperatures;
+
+import com.recruitment.temperatures.git.GitRepository;
+import com.recruitment.temperatures.models.CommitMessage;
+import com.recruitment.temperatures.models.FileName;
+import com.recruitment.temperatures.models.GitDiffLine;
+import com.recruitment.temperatures.temperatures.LineChangesHandler;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class ServerStartupChangeDetector {
+    private static final String CHANGE_DETECTION_COMMIT_MESSAGE = "Server startup Change Detection";
+    private final GitRepository gitRepository;
+    private final RecruitmentChallengeCSVProperties recruitmentChallengeCSVProperties;
+    private final LineChangesHandler lineChangesHandler;
+
+    public ServerStartupChangeDetector(GitRepository gitRepository, RecruitmentChallengeCSVProperties recruitmentChallengeCSVProperties, LineChangesHandler lineChangesHandler1) {
+        this.gitRepository = gitRepository;
+        this.recruitmentChallengeCSVProperties = recruitmentChallengeCSVProperties;
+        this.lineChangesHandler = lineChangesHandler1;
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void sync() throws GitAPIException, IOException {
+        Status status = gitRepository.status();
+        if (status.getModified().contains(recruitmentChallengeCSVProperties.getFileName())) {
+            gitRepository.commit(new FileName(recruitmentChallengeCSVProperties.getFileName()), new CommitMessage(CHANGE_DETECTION_COMMIT_MESSAGE));
+            List<String> lines = gitRepository.headEdits(new FileName(recruitmentChallengeCSVProperties.getFileName()));
+            lines.forEach(val -> lineChangesHandler.handle(new GitDiffLine(val)));
+        }
+    }
+}
---
+++ src/main/java/com/recruitment/temperatures/camel/RouteDefinitions.java
@@ -0,0 +1,39 @@
+package com.recruitment.temperatures.camel;
+
+import com.recruitment.temperatures.RecruitmentChallengeCSVProperties;
+import com.recruitment.temperatures.git.GitRepository;
+import com.recruitment.temperatures.models.GitDiffLine;
+import com.recruitment.temperatures.temperatures.LineChangesHandler;
+import com.recruitment.temperatures.models.CommitMessage;
+import com.recruitment.temperatures.models.FileName;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.List;
+
+@Component
+public class RouteDefinitions extends RouteBuilder {
+    private final GitRepository gitRepository;
+    private final RecruitmentChallengeCSVProperties recruitmentChallengeCSVProperties;
+    private final LineChangesHandler linesChangeHandler;
+    private static final String CHANGE_DETECTION_COMMIT_MESSAGE = "Apache Camel Runtime Change Detection";
+
+    public RouteDefinitions(GitRepository gitRepository, RecruitmentChallengeCSVProperties recruitmentChallengeCSVProperties, LineChangesHandler linesChangeHandler) {
+        this.gitRepository = gitRepository;
+        this.recruitmentChallengeCSVProperties = recruitmentChallengeCSVProperties;
+        this.linesChangeHandler = linesChangeHandler;
+    }
+
+    @Override
+    public void configure() {
+
+        from("file-watch:" + recruitmentChallengeCSVProperties.getDirectory() + "?events=MODIFY")
+                .filter(val -> val.getIn().getBody(File.class).getName().equals(recruitmentChallengeCSVProperties.getFileName()))
+                .process(exchange -> {
+                    gitRepository.commit(new FileName(recruitmentChallengeCSVProperties.getFileName()), new CommitMessage(CHANGE_DETECTION_COMMIT_MESSAGE));
+                    List<String> lines = gitRepository.headEdits(new FileName(recruitmentChallengeCSVProperties.getFileName()));
+                    lines.forEach(val -> linesChangeHandler.handle(new GitDiffLine(val)));
+                });
+    }
+}
