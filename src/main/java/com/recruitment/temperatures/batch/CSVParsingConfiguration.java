50c764bf94815cd134d51a51e142cb00aa6513b2
Marek Sagan <marek.sagan@protonmail.com>
2024-07-05 09:35:35 +0100

    feat(initial-csv-reader): spring-batch configuration

---
+++ src/main/java/com/recruitment/temperatures/batch/CSVParsingConfiguration.java
@@ -0,0 +1,97 @@
+package com.recruitment.temperatures.batch;
+
+import com.recruitment.temperatures.RecruitmentChallengeCSVProperties;
+import com.recruitment.temperatures.models.TemperatureModel;
+import com.recruitment.temperatures.temperatures.Temperature;
+import com.recruitment.temperatures.temperatures.TemperatureRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.text.SimpleDateFormat;
+
+@Configuration
+public class CSVParsingConfiguration {
+    private final RecruitmentChallengeCSVProperties recruitmentChallengeCSVProperties;
+    private final DataSource dataSource;
+    static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    private final TemperatureRepository temperatureRepository;
+
+    public CSVParsingConfiguration(RecruitmentChallengeCSVProperties recruitmentChallengeCSVProperties, DataSource dataSource, TemperatureRepository temperatureRepository) {
+        this.recruitmentChallengeCSVProperties = recruitmentChallengeCSVProperties;
+        this.dataSource = dataSource;
+        this.temperatureRepository = temperatureRepository;
+    }
+
+    @Bean
+    public FlatFileItemReader<TemperatureModel> reader() {
+        FlatFileItemReader<TemperatureModel> reader = new FlatFileItemReader<>();
+        reader.setResource(new FileSystemResource(recruitmentChallengeCSVProperties.getDirectory() + "/" + recruitmentChallengeCSVProperties.getFileName()));
+        reader.setLinesToSkip(0);
+        reader.setLineMapper(lineMapper());
+        return reader;
+    }
+
+    private LineMapper<TemperatureModel> lineMapper() {
+        DefaultLineMapper<TemperatureModel> defaultLineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+        lineTokenizer.setDelimiter(recruitmentChallengeCSVProperties.getDelimiter());
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("city", "date", "value");
+        BeanWrapperFieldSetMapper<TemperatureModel> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(TemperatureModel.class);
+
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return defaultLineMapper;
+    }
+    @Bean
+    public JdbcBatchItemWriter<Temperature> writer() {
+        JdbcBatchItemWriter<Temperature> writer = new JdbcBatchItemWriter<>();
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+        writer.setSql("INSERT INTO TEMPERATURES (ID,CITY,DATE,VALUE) " +
+                "VALUES (:id,:city,:date,:value)");
+        writer.setDataSource(dataSource);
+        return writer;
+    }
+    @Bean
+    public Job sampleJob(JobRepository jobRepository, Step sampleStep) {
+        return new JobBuilder("CSVLoadJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(sampleStep)
+                .build();
+    }
+    @Bean
+    public ItemProcessor<TemperatureModel, Temperature> processor() {
+        return val -> new Temperature(temperatureRepository.getNextSequence(), val.getCity(), simpleDateFormat.parse(val.getDate()), Double.parseDouble(val.getValue()));
+    }
+    @Bean
+    public Step importData(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("CSVLoadStep",jobRepository)
+                .allowStartIfComplete(true)
+                .<TemperatureModel,Temperature> chunk(100,platformTransactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+}
---
+++ src/main/java/com/recruitment/temperatures/batch/StartupCSVLoader.java
@@ -0,0 +1,51 @@
+package com.recruitment.temperatures.batch;
+
+import com.recruitment.temperatures.RecruitmentChallengeCSVProperties;
+import com.recruitment.temperatures.git.GitRepository;
+import com.recruitment.temperatures.models.CommitMessage;
+import com.recruitment.temperatures.models.FileName;
+import com.recruitment.temperatures.models.GitDiffLine;
+import com.recruitment.temperatures.temperatures.LineChangesHandler;
+import com.recruitment.temperatures.temperatures.TemperatureRepository;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+
+@Component
+public class StartupCSVLoader {
+    private final JobLauncher jobLauncher;
+    private final Job job;
+    private final GitRepository gitRepository;
+    private final RecruitmentChallengeCSVProperties recruitmentChallengeCSVProperties;
+    private static final String INITIAL_BATCH_COMMIT = "initial csv file batch persistence";
+
+
+    public StartupCSVLoader(JobLauncher jobLauncher, Job job, GitRepository gitRepository, RecruitmentChallengeCSVProperties recruitmentChallengeCSVProperties) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+        this.gitRepository = gitRepository;
+        this.recruitmentChallengeCSVProperties = recruitmentChallengeCSVProperties;
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void init() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException, GitAPIException, IOException {
+        if (Objects.isNull(gitRepository.latestObjectId())) {
+            jobLauncher.run(job, new JobParameters());
+            gitRepository.commit(new FileName(recruitmentChallengeCSVProperties.getFileName()), new CommitMessage(INITIAL_BATCH_COMMIT));
+        }
+    }
+}
